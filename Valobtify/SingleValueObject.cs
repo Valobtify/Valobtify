1512ae8998a69ae8f62a0da3825ab1b01aa82979
Mobin Valikhani <103781147+M0BIN-V@users.noreply.github.com>
2024-06-11 00:00:30 +0330

    Add project files.

---
+++ Valobtify/SingleValueObject.cs
@@ -0,0 +1,24 @@
+﻿namespace Valobtify
+{
+    public abstract class SingleValueObject<TValue> : ValueObject
+    where TValue : class
+    {
+        public virtual required TValue Value { get; init; }
+
+        public override IEnumerable<object> GetAtomicValues()
+        {
+            yield return Value;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString() ??
+                throw new NullReferenceException("value is null");
+        }
+
+        public static explicit operator string(SingleValueObject<TValue> value)
+        {
+            return value.ToString();
+        }
+    }
+}
