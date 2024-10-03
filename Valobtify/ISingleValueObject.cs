cf65b46305f7d642b7f7485f76710b922d48660f
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-10-04 00:42:18 +0330

    feat: add Create method

---
+++ Valobtify/ISingleValueObject.cs
@@ -0,0 +1,9 @@
+﻿using Resulver;
+
+namespace Valobtify;
+
+public interface ICreatableValueObject<TValueObject, TValue>
+    where TValueObject : ICreatableValueObject<TValueObject, TValue>
+{
+    public static abstract Result<TValueObject> Create(TValue value);
+}
--- Valobtify/SingleValueObject.cs
+++ Valobtify/SingleValueObject.cs
@@ -1,13 +1,18 @@
 ﻿namespace Valobtify;
 
-public abstract class SingleValueObject<TValue> : ValueObject
+public abstract class SingleValueObject<TSingleValueObject, TValue> : ValueObject
+    where TSingleValueObject : SingleValueObject<TSingleValueObject, TValue>, ICreatableValueObject<TSingleValueObject, TValue>
+    where TValue : notnull
 {
-    public virtual required TValue Value { get; init; }
+    public virtual TValue Value { get; }
+
+    protected SingleValueObject(TValue value) => Value = value;
 
     public override IEnumerable<object?> GetAtomicValues()
     {
         yield return Value;
     }
+
     public override string? ToString()
     {
         if (Value is not null)
@@ -15,10 +20,5 @@ public abstract class SingleValueObject<TValue> : ValueObject
             return Value.ToString();
         }
         return null;
-    }
-
-    public static explicit operator string(SingleValueObject<TValue> value)
-    {
-        return value.ToString() ?? "";
     }
 }
