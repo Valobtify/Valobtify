bd36df682e2d50eb5a772709cb8d4a9f63d1d6b8
Mobin Valikhani <mobinvalikhani@gmail.com>
2024-11-27 16:19:39 +0330

    fix

--- Valobtify/ICreatableValueObject.cs
+++ Valobtify/ICreatableValueObject.cs
@@ -4,6 +4,7 @@ namespace Valobtify;
 
 public interface ICreatableValueObject<TValueObject, in TValue>
     where TValueObject : ICreatableValueObject<TValueObject, TValue>
+    where TValue : notnull
 {
-    public static abstract Result<TValueObject> Create(TValue? value);
+    public static abstract Result<TValueObject> Create(TValue value);
 }
